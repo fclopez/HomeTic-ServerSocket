8f91e6debd7d7b79bb9d9c39461a2071152ff969
Fabio López C <fclopez4@hotmail.com>
2016-10-29 11:42:14 -0500

    se arregla el render html

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ var server = express();
 var websockets = require('./ws');
 
 /*se regresa al directorio padre y entra al directorio app*/
-server.use(express.static(__dirname + '/app'));
+//server.use(express.static(__dirname + '/app'));
 
-http.createServer(server).listen(3000,function(){
+//assuming app is express Object.
+server.get('/', function(req, res) {
+    res.sendFile(__dirname + '/app/index.html');
+});
+
+http.createServer(server).listen(8000,function(){
 	console.log('Servidor en el puerto %d',this.address().port);
 	/*uso el modulo importado previamente y this servidor lo envio al ws*/
 	websockets(this);
