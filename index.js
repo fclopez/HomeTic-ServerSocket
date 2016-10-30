d7246ed8ce2461ae729277085744246b5fa7b71b
Fabio López C <fclopez4@hotmail.com>
2016-10-30 02:14:54 -0500

    se coloca rutas estaticas 2

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 /*dependencias*/
+//var path = require('path');
 var http = require('http');
 var express = require('express');
-var path = require('path');
-var app = express();
+var websockets = require('./ws');
 
 /*variables globales*/
-var port = process.env.PORT || 8000
-var websockets = require('./ws');
-/*var index = path.join(__dirname, 'index.html');*/
+var app = express();
+var port = process.env.PORT || 8000;
 
-/*funcion callback
-app.use( function(req, res){
-	res.sendFile(index)
-});*/
+/*aqui se puede usar un callback si se desea como response de un request
+var index = path.join(__dirname, 'index.html')
+app.use( function(req, res){ res.sendFile(index)})*/
 
 app.use(express.static(__dirname + "/"));
 var server = http.createServer(app)
@@ -21,8 +19,3 @@ server.listen(port,function(){
 	websockets(this);
 	console.log('servidor escuchando en el puerto '+port);
 });
-
-/*app.listen(port,function(){
-	websockets(this);
-	console.log('servidor escuchando en el puerto '+port)
-});*/
