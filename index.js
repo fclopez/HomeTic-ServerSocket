82178b984875b40f9e0ffcc5dd044ff54478193c
Fabio López C <fclopez4@hotmail.com>
2016-10-30 01:26:12 -0500

    Se corrigen errores en el html

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-/*importo el modulo ws*/
+/*variables globales*/
 var port = process.env.PORT || 8000
 var websockets = require('./ws');
-
-/*se regresa al directorio padre y entra al directorio app*/
-/*app.use(express.static(__dirname + '/index.html'));*/
 var index = path.join(__dirname, 'index.html');
 
+/*funcion callback*/
 app.use( function(req, res){
 	res.sendFile(index)
 });
@@ -21,10 +19,4 @@ app.listen(port,function(){
 	console.log('servidor escuchando en el puerto '+port)
 });
 
-/*var server = http.createServer(app);
-
-server.listen(port,function(){
-	websockets(this);
-});*/
-
-/*console.log('Servidor activo en el puerto %d',port);*/
+/*var server = http.createServer(app);*/
