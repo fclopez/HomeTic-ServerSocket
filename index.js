63edfb08f128e7e99cc8121afab5caed355b9177
Fabio López C <fclopez4@hotmail.com>
2016-10-30 01:20:19 -0500

    Se reconfigura el servidor en base al ejemplo de heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*dependencias*/
 var http = require('http');
 var express = require('express');
+var path = require('path');
 var app = express();
 
 /*importo el modulo ws*/
@@ -8,11 +9,22 @@ var port = process.env.PORT || 8000
 var websockets = require('./ws');
 
 /*se regresa al directorio padre y entra al directorio app*/
-app.use(express.static(__dirname + '/'));
+/*app.use(express.static(__dirname + '/index.html'));*/
+var index = path.join(__dirname, 'index.html');
 
-var server = http.createServer(app);
+app.use( function(req, res){
+	res.sendFile(index)
+});
+
+app.listen(port,function(){
+	websockets(this);
+	console.log('servidor escuchando en el puerto '+port)
+});
+
+/*var server = http.createServer(app);
 
 server.listen(port,function(){
 	websockets(this);
-	console.log('Servidor activo en el puerto %d',port);
-});
+});*/
+
+/*console.log('Servidor activo en el puerto %d',port);*/
